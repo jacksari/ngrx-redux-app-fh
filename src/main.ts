06a20236b960c8f60d9c41d096452f9b2c10a516
Jack Sari <janasarii@gmail.com>
2022-07-01 01:54:01 -0500

    start commit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// console.log('ENV', environment);
+
+
 if (environment.production) {
   enableProdMode();
 }
